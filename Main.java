927349ed312c426da1e9525f80f3105801d053bf
syntax001 <71074471+syntax001@users.noreply.github.com>
2021-03-30 21:36:19 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) {
+        Controller mainController = new Controller();
+        mainController.playBlackjack();
+    }
+}
